9a1798ae08fcb52040b1446458176e189ca6051a
Abayomi Mustapha <mustaphaabayomi71@gmail.com>
2023-03-19 16:52:05 +0100

    todo input implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Inputfield from './components/Inputfield';
 
-function App() {
+const App: React.FC = () => {
+
+  const [todo, setTodo] = React.useState<string | number>("");
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <span className='heading'>Tasktify</span>
+      <Inputfield todo={todo} setTodo={setTodo} />
     </div>
   );
 }
